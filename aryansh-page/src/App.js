265f7f254b8feb1ce97b5366d8dc17a82c24f6a0
Aryansh <aryanshs@buffalo.edu>
2023-12-26 13:40:45 -0500

    almost done

--- aryansh-page/src/App.js
+++ aryansh-page/src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import React from "react";
 import Home from "./Components/Home/Home";
+import Projects from "./Components/Projects/Projects";
+import Experience from "./Components/Experience/Experience";
 import { Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
@@ -10,6 +12,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
       </Routes>
     </div>
   );
